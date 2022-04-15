bdcae9cb94c64fef3ea5d93b8a0ee81c5517e402
Gigacee <gigacee@gigacreation.jp>
2022-04-15 20:13:27 +0900

    Add the extensions script

--- Assets/UnityUndoExtensions/Runtime/UndoExtensions.cs
+++ Assets/UnityUndoExtensions/Runtime/UndoExtensions.cs
@@ -1,6 +1,59 @@
-﻿namespace UndoExtensions
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+using Object = UnityEngine.Object;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace GigaceeTools
 {
-    public class UndoExtensions
+    [UsedImplicitly(ImplicitUseTargetFlags.Members)]
+    public static class UndoExtensions
     {
+        public static void DoActionAsUndoable<T>(this T self, string name, Action<T> undoableAction) where T : Object
+        {
+#if UNITY_EDITOR
+            if (!EditorApplication.isPlaying)
+            {
+                Undo.RecordObject(self, name);
+            }
+#endif
+
+            undoableAction(self);
+
+#if UNITY_EDITOR
+            if (!EditorApplication.isPlaying)
+            {
+                EditorUtility.SetDirty(self);
+            }
+#endif
+        }
+
+        public static T AddComponentAsUndoable<T>(this GameObject self) where T : Component
+        {
+#if UNITY_EDITOR
+            // ReSharper disable once ConvertIfStatementToReturnStatement
+            if (!EditorApplication.isPlaying)
+            {
+                return Undo.AddComponent<T>(self);
+            }
+#endif
+
+            return self.AddComponent<T>();
+        }
+
+        public static void DestroyAsUndoable(this Object self)
+        {
+#if UNITY_EDITOR
+            if (!EditorApplication.isPlaying)
+            {
+                Undo.DestroyObjectImmediate(self);
+                return;
+            }
+#endif
+
+            Object.Destroy(self);
+        }
     }
 }
