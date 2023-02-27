dd0f698789e28cb114ce000a3c37afbd4016b1ad
Gigacee <gigacee@gigacreation.jp>
2023-02-27 21:01:00 +0900

    Add document comments

--- Assets/UndoExtensions/Runtime/GameObjectExtensions.cs
+++ Assets/UndoExtensions/Runtime/GameObjectExtensions.cs
@@ -9,6 +9,13 @@ namespace GigaCreation.Tools.UndoExtensions
 {
     public static class GameObjectExtensions
     {
+        /// <summary>
+        /// Adds a component to the GameObject.
+        /// If in editor and not play mode, registers an undo operation for this action.
+        /// </summary>
+        /// <param name="self">The GameObject you want to add the component to.</param>
+        /// <typeparam name="T">The type of component you want to add.</typeparam>
+        /// <returns>Component The newly added component.</returns>
         public static T AddComponentAsUndoable<T>(this GameObject self) where T : Component
         {
 #if UNITY_EDITOR
--- Assets/UndoExtensions/Runtime/ObjectExtensions.cs
+++ Assets/UndoExtensions/Runtime/ObjectExtensions.cs
@@ -8,6 +8,32 @@ namespace GigaCreation.Tools.UndoExtensions
 {
     public static class ObjectExtensions
     {
+        /// <summary>
+        /// Destroys the object.
+        /// If in editor and not play mode, records an undo operation so that it can be recreated.
+        /// </summary>
+        /// <param name="self">The object that will be destroyed.</param>
+        public static void DestroyAsUndoable(this Object self)
+        {
+#if UNITY_EDITOR
+            if (!EditorApplication.isPlaying)
+            {
+                Undo.DestroyObjectImmediate(self);
+                return;
+            }
+#endif
+
+            Object.Destroy(self);
+        }
+
+        /// <summary>
+        /// Execute the action.
+        /// If in editor and not play mode, records any changes done on the object.
+        /// </summary>
+        /// <param name="self">The reference to the object you will be modifying.</param>
+        /// <param name="name">The title of the action to appear in the undo history (i.e. visible in the undo menu).</param>
+        /// <param name="undoableAction">The action you want to register an undo operation.</param>
+        /// <typeparam name="T">The type of the reference to the object you will be modifying.</typeparam>
         public static void DoActionAsUndoable<T>(this T self, string name, Action<T> undoableAction) where T : Object
         {
 #if UNITY_EDITOR
@@ -25,19 +51,6 @@ namespace GigaCreation.Tools.UndoExtensions
                 EditorUtility.SetDirty(self);
             }
 #endif
-        }
-
-        public static void DestroyAsUndoable(this Object self)
-        {
-#if UNITY_EDITOR
-            if (!EditorApplication.isPlaying)
-            {
-                Undo.DestroyObjectImmediate(self);
-                return;
-            }
-#endif
-
-            Object.Destroy(self);
         }
     }
 }
